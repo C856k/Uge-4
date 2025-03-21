47376e29ee038aa63616e22e94b1f15c3c2464a3
Carl856k <113089796+C856k@users.noreply.github.com>
2025-03-21 10:18:28 +0100

    Made some changes to CRUD and MySQL connector

--- MySQL-Connector.py
+++ MySQL-Connector.py
@@ -1,9 +1,30 @@
 import mysql.connector
+from db_config import db_config 
 
-database = mysql.connector.connect(
+'''
+database = mysql.connector.connect()(
+    
     host = "localhost",
     user = "root",
-    passwd = "Velkommen25"
+    passwd = "Velkommen25"'
+    
+    host = db_config['host'],
+    user = db_config['user'],
+    passwd = db_config['passwd'],
+    db = db_config['db']
+)
+
+
+print(database)
+
+database.close()'
+'''
+
+database = mysql.connector.connect(
+    host = db_config['host'],
+    user = db_config['user'],
+    passwd = db_config['passwd'],
+    db = db_config['db']
 )
 
 print(database)
